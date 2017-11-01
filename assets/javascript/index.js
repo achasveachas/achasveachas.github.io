e8e357b09c4f6f0cf53541e60846708b1b0ae912
Yechiel Kalmenson <contact@yechiel.me>
2017-11-01 15:22:42 -0400

    take url encoding into account

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,4 @@
 if (window.location.search) {
-    var company = window.location.search.split("=")[1]
+    var company = decodeURI(window.location.search.split("=")[1])
     alert("Hello " + company + "!\n\nYou should totally hire Yechiel, I mean isn't he awesome?\n\nBut don't take my word for it, I'm just a simple browser, instead why don't you head over to the Projects page and take a look at some of the cool stuff he's made (or the resume if you wan't the boring version ;)\n\nSo waddaya say?")
 }
