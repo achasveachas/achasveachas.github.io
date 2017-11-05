dc6f01abb0d0f2f01bcd14dce0639b5369f16798
Yechiel Kalmenson <contact@yechiel.me>
2017-11-04 21:20:42 -0400

    update popup text

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -9,5 +9,5 @@ console.log("LinkdIn: https://www.linkedin.com/in/yechiel-k")
 
 if (window.location.search) {
     const company = decodeURI(window.location.search.split("=")[1])
-    setTimeout(() =>     alert("Hello " + company + "!\n\nYou should totally hire Yechiel, I mean isn't he awesome?\n\nBut don't take my word for it, I'm just a simple browser, instead why don't you head over to the Projects page and take a look at some of the cool stuff he's made (or the resume if you want the boring version ;)\n\nSo waddaya say?"), 500)
+    setTimeout(() =>     alert("Hello " + company + "!\n\nYou should totally hire Yechiel, I mean he's totally awesome!\n\nBut don't take my word for it, I'm just a simple browser, instead why don't you head over to the Projects page and take a look at some of the cool stuff he's made (or the resume if you want the boring version ;)\n\nSo waddaya say?"), 500)
 }
