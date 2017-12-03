fa72a6e7974c2376c73f87e5e1d046076f8b5046
Yechiel Kalmenson <contact@yechiel.me>
2017-12-03 11:21:37 -0500

    fix typos

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -5,8 +5,8 @@ if (window.location.search) {
 
     let message
 
-    if(company === resume) {
-        message = "Hi there,\n\nThanks fot taking the time to look at my resume!\n\nAs you know, resumes are nototrious for trying to pack in as much information as possible into as small a space as possible, for this reason my resume might be missing a lot of interesting information.\n\nThat's what this sight is for! Feel free to look around, the Projects page contains some of the stuff I built that didn't make it into the resume.\n\nThanks for stopping by, and if you want to get in touch head over to the Contact page.\nYechiel"
+    if(company === "resume") {
+        message = "Hi there,\n\nThanks for taking the time to look at my resume!\n\nFeel free to look around. The Projects page contains some of the stuff I built that didn't make it into the resume.\n\nThanks for stopping by, and if you want to get in touch please head over to the Contact page.\nYechiel"
     } else {
         message = "Hello " + company + "!\n\nYou should totally hire Yechiel, I mean he's totally awesome!\n\nBut don't take my word for it, I'm just a simple browser, instead why don't you head over to the Projects page and take a look at some of the cool stuff he's made (or the resume if you want the boring version ;)\n\nSo waddaya say?"
     }
